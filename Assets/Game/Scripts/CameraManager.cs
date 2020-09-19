3b47b1b27775de0fdf6140862e56d2cae0f87d69
Soohyun, JEON <inputjsh@gmail.com>
2020-09-19 13:28:50 +0900

    카메라 무빙 변경

--- Assets/Game/Scripts/CameraManager.cs
+++ Assets/Game/Scripts/CameraManager.cs
@@ -14,22 +14,11 @@ public class CameraManager : MonoBehaviour
 
     private void Update()
     {
-        //transform.position = new Vector3(cameraPos.position.x,
-        //                                30.9f,
-        //                                cameraPos.position.z - 13.8f);
-
         Vector3 vec = new Vector3(MyPos.position.x + Init_CameraPos.x,
                                    Init_CameraPos.y,
                                    MyPos.position.z + Init_CameraPos.z);
-        transform.position = Vector3.Lerp(transform.position, vec, Time.deltaTime * 1f);
 
-
-        //transform.Translate(new Vector3(cameraPos.position.x,
-        //                                0,
-        //                                cameraPos.position.z) * Time.deltaTime, Space.World);
-        //transform.position += new Vector3(cameraPos.position.x,
-        //                                  0,
-        //                                  cameraPos.position.z) * Time.deltaTime;
+        transform.position = vec;
 
     }
 
@@ -44,27 +33,6 @@ public class CameraManager : MonoBehaviour
         Vector3 vec = new Vector3(MyPos.position.x + Init_CameraPos.x,
                                   Init_CameraPos.y,
                                   MyPos.position.z + Init_CameraPos.z);
-
-        //Vector3 vec = new Vector3(0, 30.9f, -16.32f);
-
-        //Vector3 vec = new Vector3(MyPos.position.x, 30.9f, MyPos.position.z - 16.32f);
-
-        //while (true)
-        //{
-        //    transform.localPosition = vec + new Vector3(Random.Range(0, 11f), 0, Random.Range(0, 11f)) * 10;
-        //    //transform.position = MyPos.position;
-        //    //Debug.Log(new Vector3(Random.Range(0, 11f), 0, Random.Range(0, 11f)) * 10);
-        //    //Debug.Log(transform.position);
-        //    //timer += Time.deltaTime;
-        //    //Debug.Log(timer);
-        //    if (timer >= 60f)
-        //    {
-        //        Debug.Log("ddd");
-        //        transform.position = vec;
-        //        yield break;
-        //    }
-        //}
-
         while (timer > 0f)
         {
             timer -= 0.05f;
