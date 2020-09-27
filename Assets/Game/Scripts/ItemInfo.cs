1e4536c7e1b4d5230ae7a8510f8289d5656ee2e6
Soohyun, JEON <inputjsh@gmail.com>
2020-09-27 14:47:26 +0900

    수현작업

---
+++ Assets/Game/Scripts/ItemInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Item_1,
+    Item_2,
+    Item_3
+}
+
+public class ItemInfo : MonoBehaviour
+{
+    public ItemType ItemType;
+
+    void Start()
+    {
+        // ItemType = ItemType.Item_1;
+    }
+
+
+}
