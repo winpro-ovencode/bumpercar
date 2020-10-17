f8c300cdf5349eac8301744ca20c2189f71567fb
DEUK CHEON, OH <guruwang2@gmail.com>
2020-10-17 10:19:22 +0900

    인벤 추가

---
+++ Assets/Game/Scripts/Inven/ArrangeSlot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ArrangeSlot : MonoBehaviour
+{
+    public GameObject Slot;
+    public int row=5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int col = gameObject.GetComponent<GridLayoutGroup>().constraintCount;
+
+        for (int i = 0; i < row * col; i++)
+        {
+            GameObject slot = Instantiate(Slot);
+            slot.transform.parent = transform;
+        }
+    }
+
+    void Init()
+    {
+
+    }
+
+    private void MakeSlots()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Game/Scripts/Inven/Slot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    private int index;
+    public void SetIndex(int index){
+        this.index = index;
+    }
+}
